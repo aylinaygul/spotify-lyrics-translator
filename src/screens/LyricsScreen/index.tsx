f158dc12bf0e4213b255ef306d89efaa85ddfa2d
Aylin Aygül <74706468+aylinaygul@users.noreply.github.com>
2025-01-11 00:12:44 +0300

    Lyrics screen design

--- src/screens/LyricsScreen/index.tsx
+++ src/screens/LyricsScreen/index.tsx
@@ -2,24 +2,35 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchLyrics } from '../../redux/thunks';
 import { AppDispatch, RootState } from '../../redux/store';
-import { View, Text, ActivityIndicator, Button } from 'react-native';
+import { View, Text, ActivityIndicator, Button, ScrollView } from 'react-native';
+import styles from './styles';
 
 const LyricsScreen = () => {
     const dispatch = useDispatch<AppDispatch>();
     const { lyrics, isLoading, error } = useSelector((state: RootState) => state.lyrics);
 
     useEffect(() => {
-        dispatch(fetchLyrics('Shape of You Ed Sheeran'));
+        dispatch(fetchLyrics(''));
     }, [dispatch]);
 
     if (isLoading) return <ActivityIndicator />;
     if (error) return <Text>Error: {error}</Text>;
 
     return (
-        <View>
-            <Text>{lyrics || 'No lyrics available'}</Text>
-            <Button title="Translate" onPress={() => {/* Navigate to translation screen */ }} />
-        </View>
+        <View style={styles.container}>
+
+            <View style={{ flex: 1 }}>
+                <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Lyrics</Text>
+            </View>
+            <View style={[styles.lyricsBox, { flex: 13 }]}>
+                <ScrollView>
+                    <Text>{lyrics || 'No lyrics available'}</Text>
+                </ScrollView>
+            </View>
+            <View style={{ flex: 1 }}>
+                <Button title="Translate" onPress={() => {/* Navigate to translation screen} /> */ }} />
+            </View>
+        </View >
     );
 };
 
---
+++ src/screens/LyricsScreen/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'stretch',
+        padding: 10,
+        gap: 10,
+    },
+    lyricsBox: {
+        backgroundColor: 'lightgrey',
+        padding: 10,
+    }
+});
+
+export default styles;
